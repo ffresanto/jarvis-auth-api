c1bfc0b8f42bdf8adc9f06bc99bbffba38d81b8f
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2024-06-19 21:42:15 -0300

    feat: add sqlite in the project

---
+++ src/JarvisAuth.API/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace JarvisAuth.API.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    [Produces("application/json")]
+    public class HealthController(HealthCheckService healthCheckService) : Controller
+    {
+        [HttpGet]
+        [SwaggerOperation(Summary = "Checks the status and integrity of the API's resources and services")]
+        [SwaggerResponse(500, "An unexpected error occurred while processing your request.", typeof(string))]
+        public async Task<ActionResult> GetHealthCheck()
+        {
+            return Ok(await healthCheckService.CheckHealthAsync());
+        }
+    }
+}
--- src/JarvisAuth.API/Program.cs
+++ src/JarvisAuth.API/Program.cs
@@ -1,15 +1,17 @@
+using HealthChecks.UI.Client;
+using JarvisAuth.Infrastructure.Configurations.Contexts;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqliteDbContextAndHealthCheck(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +19,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+app.MapHealthChecks("/api/healthz", new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
 
 app.Run();
