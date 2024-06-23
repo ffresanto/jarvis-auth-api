7df5ea8b26a22a25a40180875d03055ef5535967
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2024-06-23 12:44:03 -0300

    feat: implements tests

--- tests/JarvisAuth.Tests/JarvisAuth.Application.Tests/Services/JarvisServiceTests.cs
+++ tests/JarvisAuth.Tests/JarvisAuth.Application.Tests/Services/JarvisServiceTests.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using JarvisAuth.Application.Services;
 using JarvisAuth.Core.Messages;
+using JarvisAuth.Core.Requests.Jarvis;
 using JarvisAuth.Core.Responses.Jarvis;
+using JarvisAuth.Core.Responses.Shared;
+using JarvisAuth.Domain.Entities;
 using JarvisAuth.Domain.Interfaces.Repositories;
 using JarvisAuth.Domain.models;
 using Moq;
@@ -10,15 +13,15 @@ namespace JarvisAuth.Tests.JarvisAuth.Application.Tests.Services
 {
     public class JarvisServiceTests
     {
-        private readonly Mock<IJarvisRepository> _typesRepositoryMock;
+        private readonly Mock<IJarvisRepository> _jarvisRepositoryMock;
         private readonly Mock<IMapper> _mapperMock;
-        private readonly JarvisService _typeService;
+        private readonly JarvisService _jarvisService;
 
         public JarvisServiceTests()
         {
-            _typesRepositoryMock = new Mock<IJarvisRepository>();
+            _jarvisRepositoryMock = new Mock<IJarvisRepository>();
             _mapperMock = new Mock<IMapper>();
-            _typeService = new JarvisService(_typesRepositoryMock.Object, _mapperMock.Object);
+            _jarvisService = new JarvisService(_jarvisRepositoryMock.Object, _mapperMock.Object);
         }
 
         [Fact]
@@ -27,11 +30,11 @@ namespace JarvisAuth.Tests.JarvisAuth.Application.Tests.Services
             // Arrange
             var genderTypes = new List<GenderType> { new GenderType() };
             var mappedResponse = new List<GetGenderTypeResponse> { new GetGenderTypeResponse() };
-            _typesRepositoryMock.Setup(repo => repo.GetGenderTypes()).ReturnsAsync(genderTypes);
+            _jarvisRepositoryMock.Setup(repo => repo.GetGenderTypes()).ReturnsAsync(genderTypes);
             _mapperMock.Setup(mapper => mapper.Map<List<GetGenderTypeResponse>>(genderTypes)).Returns(mappedResponse);
 
             // Act
-            var result = await _typeService.GetGendersTypes();
+            var result = await _jarvisService.GetGendersTypes();
 
             // Assert
             Assert.NotNull(result.Data);
@@ -43,10 +46,10 @@ namespace JarvisAuth.Tests.JarvisAuth.Application.Tests.Services
         public async Task GetGendersTypes_ReturnsNotFound_WhenDataDoesNotExist()
         {
             // Arrange
-            _typesRepositoryMock.Setup(repo => repo.GetGenderTypes()).ReturnsAsync((List<GenderType>)null);
+            _jarvisRepositoryMock.Setup(repo => repo.GetGenderTypes()).ReturnsAsync((List<GenderType>)null);
 
             // Act
-            var result = await _typeService.GetGendersTypes();
+            var result = await _jarvisService.GetGendersTypes();
 
             // Assert
             Assert.Null(result.Data);
@@ -60,11 +63,11 @@ namespace JarvisAuth.Tests.JarvisAuth.Application.Tests.Services
             // Arrange
             var documentTypes = new List<DocumentType> { new DocumentType() };
             var mappedResponse = new List<GetDocumentTypeResponse> { new GetDocumentTypeResponse() };
-            _typesRepositoryMock.Setup(repo => repo.GetDocumentTypes()).ReturnsAsync(documentTypes);
+            _jarvisRepositoryMock.Setup(repo => repo.GetDocumentTypes()).ReturnsAsync(documentTypes);
             _mapperMock.Setup(mapper => mapper.Map<List<GetDocumentTypeResponse>>(documentTypes)).Returns(mappedResponse);
 
             // Act
-            var result = await _typeService.GetDocumentsTypes();
+            var result = await _jarvisService.GetDocumentsTypes();
 
             // Assert
             Assert.NotNull(result.Data);
@@ -76,15 +79,44 @@ namespace JarvisAuth.Tests.JarvisAuth.Application.Tests.Services
         public async Task GetDocumentsTypes_ReturnsNotFound_WhenDataDoesNotExist()
         {
             // Arrange
-            _typesRepositoryMock.Setup(repo => repo.GetDocumentTypes()).ReturnsAsync((List<DocumentType>)null);
+            _jarvisRepositoryMock.Setup(repo => repo.GetDocumentTypes()).ReturnsAsync((List<DocumentType>)null);
 
             // Act
-            var result = await _typeService.GetDocumentsTypes();
+            var result = await _jarvisService.GetDocumentsTypes();
 
             // Assert
             Assert.Null(result.Data);
             Assert.Contains(GlobalMessages.RECORDS_NOT_FOUND_IN_DATABASE, result.Errors);
             Assert.Equal(404, result.StatusCode);
         }
+
+        [Fact]
+        public async Task PostCreateUserJarvis_ReturnsError_WhenEmailExists()
+        {
+            // Arrange
+            var request = new PostCreateUserJarvisRequest
+            {
+                Name = "John Doe",
+                Email = "test@example.com",
+                Password = "password123",
+                ContactNumber = "1234567890",
+                GenderTypeId = 1,
+                DocumentTypeId = 1,
+                DocumentNumber = "123456",
+            };
+
+            var response = new Response<PostCreateUserJarvisResponse>();
+
+            _jarvisRepositoryMock.Setup(repo => repo.EmailExistsAsync(request.Email)).ReturnsAsync(true);
+
+            // Act
+            var result = await _jarvisService.PostCreateUserJarvis(request);
+
+            // Assert
+            Assert.NotNull(result.Errors);
+            Assert.Contains("Email already exists", result.Errors);
+            Assert.Equal(409, result.StatusCode);
+        }
+
     }
 }
