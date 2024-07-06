5567820d3e090b89e2aa3c81d674dc1023eaa4af
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2024-07-06 19:09:16 -0300

    feat: adding Authorizar in rota create application

--- src/JarvisAuth.API/Controllers/ApplicationController.cs
+++ src/JarvisAuth.API/Controllers/ApplicationController.cs
@@ -1,11 +1,10 @@
 ﻿using JarvisAuth.API.Controllers.Base;
-using JarvisAuth.Application.Services;
 using JarvisAuth.Core.Messages;
 using JarvisAuth.Core.Requests.Application;
-using JarvisAuth.Core.Requests.Jarvis;
 using JarvisAuth.Core.Responses.Application;
 using JarvisAuth.Core.Responses.Shared;
 using JarvisAuth.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -18,12 +17,13 @@ namespace JarvisAuth.API.Controllers
     {
 
         [HttpPost("create")]
+        [Authorize]
         [SwaggerOperation(Summary = "Creates a new application for the Jarvis authentication system.")]
         [SwaggerResponse(200, GlobalMessages.OPERATION_SUCCESS_200, typeof(Response<PostCreateApplicationResponse>))]
         [SwaggerResponse(409, GlobalMessages.REQUEST_CONFLICT_409, typeof(Response<string>))]
         [SwaggerResponse(422, GlobalMessages.VALIDATION_ERRORS_422, typeof(Response<string>))]
         [SwaggerResponse(500, GlobalMessages.GLOBAL_EXCEPTION_500, typeof(Response<string>))]
-        public async Task<ActionResult> PostCreateUserJarvis(PostCreateApplicationRequest request)
+        public async Task<ActionResult> PostCreateApplication(PostCreateApplicationRequest request)
         {
             return CustomResponse(await applicationService.CreateApplication(request));
         }
